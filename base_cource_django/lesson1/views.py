16fdba5300cf4aacefc2329a82531655b51a2774
Ruslan Bihun <bigun.ruslan@gmail.com>
2025-09-04 19:12:04 +0200

    add path and view base for lesson1

--- base_cource_django/lesson1/views.py
+++ base_cource_django/lesson1/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
 # Create your views here.
+
+def index(request: HttpRequest):
+    if request.method == "POST":
+        pass
+    elif request.method == "GET":
+        pass
+    context = {
+        
+    }
+    return HttpResponse("Hello")
