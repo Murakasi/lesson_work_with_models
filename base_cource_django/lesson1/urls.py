c192f3d4558b646a805bc5bdc13240504e7f3c22
Ruslan Bihun <bigun.ruslan@gmail.com>
2025-09-04 21:23:05 +0200

    update urls and views get name

--- base_cource_django/lesson1/urls.py
+++ base_cource_django/lesson1/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from lesson1 import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("home/<str:name>/", views.index, name="index"),
 ]
--- base_cource_django/lesson1/views.py
+++ base_cource_django/lesson1/views.py
@@ -3,12 +3,13 @@ from django.http import HttpRequest, HttpResponse
 
 # Create your views here.
 
-def index(request: HttpRequest):
+def index(request: HttpRequest, name):
+    name_user = name
     if request.method == "POST":
         pass
     elif request.method == "GET":
         pass
     context = {
-        "name": "Ruslan",
+        "name": name_user,
     }
     return render(request, "lesson1/index.html", context)
